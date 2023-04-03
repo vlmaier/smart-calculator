31638f2e7ae94ca591c82c3aed3f280f16b15fa2
Vladas Maier <vladas.maier@goyahealth.de>
2023-04-03 07:33:55 +0200

    Add "JSON" to "String basics" topic

---
+++ Topics/String basics/JSON/src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("""
+        {
+            "firstName": "John",
+            "lastName": "Smith",
+            "age": 35,
+            "phoneNumbers": [
+                {
+                    "type": "mobile",
+                    "number": "123 567-7890"
+                }
+            ]
+        }
+    """.trimIndent())
+}
